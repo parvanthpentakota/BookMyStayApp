9f1cba2e1098a377046a013647121da5238af292
Parvanth Pentakota <parvanthpentakota2316@gmail.com>
2026-03-24 08:36:47 +0530

    UC6 - Booking allocation with unique room IDs and double booking prevention

--- src/RoomInventory.java
+++ src/RoomInventory.java
@@ -16,4 +16,15 @@ public class RoomInventory {
     public int getAvailability(String roomType) {
         return inventory.getOrDefault(roomType, 0);
     }
+
+    public void updateAvailability(String roomType, int count) {
+        inventory.put(roomType, count);
+    }
+
+    public void displayInventory() {
+        System.out.println("\nCurrent Inventory:");
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + " Available: " + entry.getValue());
+        }
+    }
 }
